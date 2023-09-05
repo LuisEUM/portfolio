d2e502335d49d8746db7cb6a3fa777456018cc13
¨LuisEUM¨ <luiseum95@gmail.com>
2023-09-05 10:47:41 +0200

    Deployment Failed - fixing project page

--- app/context/languageContext.tsx
+++ app/context/languageContext.tsx
@@ -1,74 +1,71 @@
-'use client'
-import { createContext, useEffect, useState } from 'react'
-import textData from '../data/text.json'
-// import { AnimatePresence, LayoutGroup, motion, useInView } from 'framer-motion'
-import { getCookie, setCookie } from 'cookies-next'
-// import DotsLoader from '../components/loaders/dotsLoader'
+"use client";
+import { createContext, useEffect, useRef, useState } from "react";
+import textData from "../data/text.json";
+import { AnimatePresence, LayoutGroup, motion, useInView } from 'framer-motion'
+import { getCookie, setCookie } from "cookies-next";
+import DotsLoader from "../components/loaders/dotsLoader";
 
-export const LanguageContext = createContext(null)
+export const LanguageContext = createContext(null);
 
 export const LanguageProvider = ({ children }) => {
-  const [languageCookie, setLanguageCookie] = useState(getCookie('language'))
-  const [text, setText] = useState(textData.es)
-  const [loading, setLoading] = useState(true)
-  // const ref = useRef(null)
-  // const isInView = useInView(ref, { once: false })
+  const [languageCookie, setLanguageCookie] = useState(getCookie("language"));
+  const [text, setText] = useState(textData.es);
+  const [loading, setLoading] = useState(true);
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: false })
 
-  // const container = {
-  //   hidden: { opacity: 0 },
-  //   show: (i = 1) => ({
-  //     opacity: 1,
-  //     transition: {
-  //       staggerChildren: 1,
-  //       delayChildren: 0.25 * i
-  //     }
-  //   })
-  // }
+  const container = {
+    hidden: { opacity: 0 },
+    show: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        staggerChildren: 1,
+        delayChildren: 0.25 * i
+      }
+    })
+  }
 
   useEffect(() => {
     if (languageCookie === undefined) {
-      setCookie('language', 'en')
+      setCookie("language", "en");
     }
-    const finalText = getText(languageCookie, textData)
-    setText(finalText)
-  }, [languageCookie])
+    const finalText = getText(languageCookie, textData);
+    setText(finalText);
+  }, [languageCookie]);
 
   return (
-    <>
-      <>
-        {/* {loading || text === null
-          ? (
-          <div
+    <LayoutGroup>
+      <AnimatePresence>
+        {loading || text === null ? (
+          <motion.div
             key="loader"
             className="h-screen grid grid-cols-1 align-middle justify-around overflow-hidden max-w-full"
-            // variants={container}
-            // initial="hidden"
-            // animate={isInView ? 'show' : 'hidden'}
-            // ref={ref}
-            // onAnimationEnd={() => setLoading(false)}
+            variants={container}
+            initial="hidden"
+            animate={isInView ? "show" : "hidden"}
+            ref={ref}
+            onAnimationEnd={() => setLoading(false)}
           >
             <DotsLoader setLoading={setLoading} />
-          </div>
-            )
-          : ( */}
+          </motion.div>
+        ) : (
           <LanguageContext.Provider value={{ text, setLanguageCookie }}>
             {children}
           </LanguageContext.Provider>
-            {/* )} */}
-      </>
-    </>
-  )
-}
+        )}
+      </AnimatePresence>
+    </LayoutGroup>
+  );
+};
 
-function getText (languageCookie, textData) {
+function getText(languageCookie, textData) {
   if (languageCookie === undefined) {
-    return textData.en
-  } else if (languageCookie.includes('en')) {
-    return textData.en
-  } else if (languageCookie.includes('es')) {
-    return textData.es
+    return textData.en;
+  } else if (languageCookie.includes("en")) {
+    return textData.en;
+  } else if (languageCookie.includes("es")) {
+    return textData.es;
   } else {
-    return textData.en
+    return textData.en;
   }
 }
-
