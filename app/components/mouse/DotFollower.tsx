c0d6bf5fd831266bcef34663265a8c4d8d07414b
¨LuisEUM¨ <luiseum95@gmail.com>
2024-02-25 15:53:24 +0100

    fixed dot

--- app/components/mouse/DotFollower.tsx
+++ app/components/mouse/DotFollower.tsx
@@ -1,12 +1,69 @@
 "use client";
 import { useState, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import useMousePosition from "@/app/hooks/useMousePosition";
 
 function DotFollower() {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = 400;
+  const [isComplete, setIsComplete] = useState(false);
+
+  const maskStyle = {
+    WebkitMaskImage: "url('../../public/mask.svg')",
+    WebkitMaskRepeat: "no-repeat",
+    WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+    WebkitMaskSize: `${size}px`,
+    background: "#ec4e39", // Este color se puede ajustar usando clases de Tailwind si es estático
+  };
+
   return (
     <div className=" ">
       <motion.div className="h-[3vw] absolute top-0 w-full bg-gradient-to-b from-[#0F0F0F] to-transparent  pointer-events-none mix-blend-luminosity" />
+      {/* <main className="">
+        <motion.div
+          className="rounded-full z-40   bg-primary  absolute  mix-blend-overlay  "
+          style={{
+            width: size,
+            height: size,
+            x: x - size / 2,
+            y: y - size / 2,
+            padding: "200px",
+          }}
+          transition={{ type: "spring", stiffness: 700, damping: 30 }}
+          initial={{
+            x: x,
+            y: y,
+            height: "0vw",
+            opacity: 1,
+            top: "-0vw",
+            left: "-0vw",
+            padding: size,
+          }}
 
+          whileTap={{
+            opacity: 1,
+            scale: 1.1,
+            filter: "brightness(1.5)",
+          }}
+          animate={{
+            x: x - (size/2) ,
+            y: y - size ,
+            padding: size/2,
+            opacity: 1,
+          }}
+          exit={{
+            opacity: 0,
+            height: 0,
+            width: 0,
+            x: 0,
+            y: 0,
+            padding: "0vw",
+          }}
+          // onAnimationComplete={() => setIsComplete(true)}
+          // onAnimationStart={() => setIsComplete(false)}
+        />
+      </main> */}
       <Box />
       <motion.div className="h-[3vw] absolute bottom-0 w-full bg-gradient-to-b from-transparent to-[#0F0F0F]  pointer-events-none mix-blend-luminosity" />
     </div>
@@ -46,8 +103,6 @@ function getRelativeCoordinates(event, referenceElement) {
   };
 }
 const Box = () => {
-  // const ref = useRef(null);
-  // const { x, y } = useFollowPointer(ref);
   const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
@@ -77,43 +132,35 @@ const Box = () => {
       <AnimatePresence>
         {isHovered && (
           <motion.div
-            className="h-[15vw] w-[15vw] rounded-full z-40   bg-primary  absolute hover:p-[7.5vw]  mix-blend-overlay  "
+            className="h-[15vw] w-[15vw] rounded-full z-40   bg-primary  absolute  mix-blend-overlay  "
             initial={{
               x: mousePosition.x,
               y: mousePosition.y,
               height: "0vw",
               width: "0vw",
               opacity: 1,
-              top: "-0vw",
-              left: "-0vw",
+              scale: 0,
               padding: "0vw",
-            }}
-            whileHover={{
-              padding: "7.5vw",
-              height: "7.5vw",
-              width: "7.5vw",
-              opacity: 1,
               top: "-7.5vw",
               left: "-7.5vw",
             }}
-            whileTap={{
-              padding: "7vw",
-              height: "7vw",
-              width: "7vw",
+            whileHover={{
+              scale: 2,
               opacity: 1,
-              top: "-7vw",
-              left: "-7vw",
+            }}
+            whileTap={{
+              opacity: 1,
+              scale: 1.8,
               filter: "brightness(1.5)",
             }}
             animate={{
               x: mousePosition.x,
               y: mousePosition.y,
-              padding: isComplete ? "7.5vw" : "0vw",
-              height: isComplete ? "7.5vw" : "3.75vw",
-              width: isComplete ? "7.5vw" : "3.75vw",
+              padding: "3.75vw",
+              scale: isComplete ? 0.8 : 0.4,
               opacity: 1,
-              top: isComplete ? "7.5vw" : "-1.875vw",
-              left: isComplete ? "7.5vw" : "-1.875vw",
+              top: "-3.75vw",
+              left: "-3.75vw",
             }}
             exit={{
               opacity: 0,
@@ -123,7 +170,6 @@ const Box = () => {
               left: "-0vw",
               padding: "0vw",
             }}
-            transition={{ type: "spring" }}
             onAnimationComplete={() => setIsComplete(true)}
             onAnimationStart={() => setIsComplete(false)}
           />
