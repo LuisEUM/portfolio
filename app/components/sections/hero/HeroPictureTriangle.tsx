99e585ec00a087b7f3517648d42505b522a2739c
¨LuisEUM¨ <luiseum95@gmail.com>
2024-03-28 10:12:44 +0100

    Image hero optimized with eager loading

--- app/components/sections/hero/HeroPictureTriangle.tsx
+++ app/components/sections/hero/HeroPictureTriangle.tsx
@@ -51,6 +51,7 @@ function HeroPictureTriangle({ srcBody, srcHead }: HeroPictureTriangleProps) {
             className="w-full h-full object-contain  cursor-none pointer-events-none"
             priority
             placeholder="blur"
+            loading="eager"
           />
         </motion.div>
       </motion.div>
@@ -61,7 +62,8 @@ function HeroPictureTriangle({ srcBody, srcHead }: HeroPictureTriangleProps) {
         sizes="contain"
         className="w-full h-full object-contain absolute top-0 cursor-none pointer-events-none"
         placeholder="blur"
-      />
+        loading="eager"
+        />
       <svg className="svg absolute w-0 h-0 ">
         <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox">
           <path d="M0.623,0.028 C0.67,-0.02,0.752,0.001,0.77,0.067 L0.884,0.478 L0.997,0.89 C1,0.955,0.956,1,0.89,0.999 L0.067,0.785 C0.001,0.768,-0.022,0.686,0.026,0.637 L0.324,0.333 L0.623,0.028"></path>
