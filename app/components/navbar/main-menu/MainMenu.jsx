93c7abb46f77d53ef1101bede9b8dda2456ee36a
¨LuisEUM¨ <luiseum95@gmail.com>
2024-06-05 14:46:33 +0200

    Fixing button bug changing places

--- app/components/navbar/main-menu/MainMenu.jsx
+++ app/components/navbar/main-menu/MainMenu.jsx
@@ -49,77 +49,76 @@ const MainMenu = ({ isOpen, toggleOpen, setModalOpenNavbar }) => {
       initial="closed"
       animate={isOpen ? "open" : "closed"}
       exit="closed"
-      className="flex-row group-first:border-b-2 top-[75px] right-0 fixed flex col-span-full  h-full  items-start  justify-center "
+      className="flex-row group-first:border-b-2 top-[75px] right-0 fixed flex col-span-full  h-[calc(100dvh-75px)]  items-start  justify-center "
       ref={ref}
     >
       <AnimatePresence mode="wait">
         {isOpen && (
-          <div className="w-full min-w-[300px] flex justify-between items-center">
-            <div className="min-w-[300px] flex flex-col w-full">
-              {routes.map((route) => (
-                <Link
-                  key={route.pathname}
-                  href={
-                    route.pathname === "/projects"
-                      ? "/projects?category=All&page=1"
-                      : route.pathname
-                  }
-                  className={`relative items-center    tracking-wider rounded-md w-full  ${
-                    currentPathname === route.pathname
-                      ? "text-primary"
-                      : "text-zinc-100  hover:scale-105 hover:text-primary transition-all"
-                  }`}
-                >
-                  <motion.div
-                    className="justify-center hover:bg-zinc-800 py-5 w-full "
-                    whileTap={{ scale: 0.9 }}
+          <div className="w-full min-w-[300px] h-full">
+            <div className="flex flex-col justify-between items-center h-full">
+              <div className="min-w-[300px] flex flex-col w-full">
+                {routes.map((route) => (
+                  <Link
+                    key={route.pathname}
+                    href={
+                      route.pathname === "/projects"
+                        ? "/projects?category=All&page=1"
+                        : route.pathname
+                    }
+                    className={`relative items-center    tracking-wider rounded-md w-full  ${
+                      currentPathname === route.pathname
+                        ? "text-primary"
+                        : "text-zinc-100  hover:scale-105 hover:text-primary transition-all"
+                    }`}
                   >
-                    <div className="w-full flex flex-row gap-5  mx-auto pl-5">
-                      <Image
-                        src={route.icon}
-                        width={32}
-                        height={32}
-                        alt="Projects folder icon"
-                        className=" fill-zinc-300 h-6 w-6 col-span-1   "
-                      />
-                      <p className="text-zinc-100 text-xl tracking-wider col-span-10 col-start-3 h-full">
-                        {route.name}
-                      </p>
-                    </div>
-                  </motion.div>
-                </Link>
-              ))}
+                    <motion.div
+                      className="justify-center hover:bg-zinc-800 py-5 w-full "
+                      whileTap={{ scale: 0.9 }}
+                    >
+                      <div className="w-full flex flex-row gap-5  mx-auto pl-5">
+                        <Image
+                          src={route.icon}
+                          width={32}
+                          height={32}
+                          alt="Projects folder icon"
+                          className=" fill-zinc-300 h-6 w-6 col-span-1   "
+                        />
+                        <p className="text-zinc-100 text-xl tracking-wider col-span-10 col-start-3 h-full">
+                          {route.name}
+                        </p>
+                      </div>
+                    </motion.div>
+                  </Link>
+                ))}
 
-              <div className="grid grid-cols-12 py-5 px-5">
-                <div className="grid col-span-12 grid-cols-12 relative w-full">
-                  <Image
-                    priority
-                    src="/icons/navbar/Translate.svg"
-                    fill
-                    sizes="fill"
-                    alt="Follow us on Twitter"
-                    className="flex h-6 w-6 self-center items-center justify-center col-span-1 fill-zinc-300 col-start-1 lg:col-start-2"
-                  />
+                <div className="grid grid-cols-12 py-5 px-5">
+                  <div className="grid col-span-12 grid-cols-12 relative w-full">
+                    <Image
+                      priority
+                      src="/icons/navbar/Translate.svg"
+                      fill
+                      sizes="fill"
+                      alt="Follow us on Twitter"
+                      className="flex h-6 w-6 self-center items-center justify-center col-span-1 fill-zinc-300 col-start-1 lg:col-start-2"
+                    />
 
-                  <p className="text-zinc-100 text-xl tracking-wider col-span-9 col-start-3 h-full">
-                    {text.menu.languages}
-                  </p>
-                </div>
-                <div className="flex col-span-9 col-start-3  my-5 gap-x-2 items-center">
-                  <p className=" text-lg tracking-wider ">ES</p>
-                  <LanguaguesSwitch className="w-16" />
-                  <p className=" text-lg tracking-wider">EN</p>
+                    <p className="text-zinc-100 text-xl tracking-wider col-span-9 col-start-3 h-full">
+                      {text.menu.languages}
+                    </p>
+                  </div>
+                  <div className="flex col-span-9 col-start-3  my-5 gap-x-2 items-center">
+                    <p className=" text-lg tracking-wider ">ES</p>
+                    <LanguaguesSwitch className="w-16" />
+                    <p className=" text-lg tracking-wider">EN</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div
-              className="  py-5 px-10 w-full  absolute bottom-28 "
-              onClick={toggleOpen}
-            >
-              <ContactModalButton
-                setModalOpenNavbar={setModalOpenNavbar}
-                className="text-center px-7  self-center border border-primary justify-center items-center gap-2.5 inline-flex hover:shadow-primary/50 hover:shadow-md w-full  rounded-3xl bg-primary col-span-10 uppercase text-zinc-900 tracking-wider font-extrabold text-lg  py-3"
-              />
+              <div className=" h-full flex pb-10 " onClick={toggleOpen}>
+                <ContactModalButton
+                  setModalOpenNavbar={setModalOpenNavbar}
+                  className="text-center px-7  self-end border border-primary justify-center items-center gap-2.5 inline-flex hover:shadow-primary/50 hover:shadow-md w-full  rounded-3xl bg-primary col-span-10 uppercase text-zinc-900 tracking-wider font-extrabold text-lg  py-3"
+                />
+              </div>
             </div>
           </div>
         )}
